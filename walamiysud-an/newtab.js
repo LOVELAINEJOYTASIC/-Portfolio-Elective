774e64efdf55380fa22399757125fc531dd46b45
LOVELAINEJOYTASIC <143611547+LOVELAINEJOYTASIC@users.noreply.github.com>
2023-12-18 09:25:37 +0800

    Add files via upload
    Hello welcome

---
+++ walamiysud-an/newtab.js
@@ -0,0 +1,85 @@
+function openfb() {
+    var url = "https://www.facebook.com/EzLawliet"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+  function opengit() {
+    var url = "https://github.com/nyannncatt"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openyt() {
+    var url = "https://www.youtube.com/channel/UCDCwtfzgDeQruL0tCNLKdkw"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+
+
+
+  function openfb2() {
+    var url = "https://www.facebook.com/lovelainejoy.tasic.3"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+  function opengit2() {
+    var url = "https://github.com/LOVELAINEJOYTASIC"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openyt2() {
+    var url = "https://www.youtube.com/@eljouy1743"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+
+
+
+
+  function openrepo() {
+    var url = "https://github.com/nyannncatt/Elective"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+  function openrepo2() {
+    var url = "https://github.com/nyannncatt/hover-link-animation"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openrepo3() {
+    var url = "https://github.com/nyannncatt/Inventorysimple"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+
+
+  function openrepo4() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/spa-jquery"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+ 
+  function openrepo5() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/Bouncing-Ball-Act"
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+  function openrepo6() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/-My-Portfolio"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+  function openrepo6() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/it24-main"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+  function openrepo6() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/walalang"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+  function openrepo6() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/LinkAmboutzz"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+  function openrepo6() {
+    var url = "https://github.com/LOVELAINEJOYTASIC/-My-Portfolio"
+    window.open(url, '_blank', 'noopener,noreferrer');
+
+  }
+
